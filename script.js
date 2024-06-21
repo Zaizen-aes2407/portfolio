296cf639b9f4af069851747fd8891b40f2e9e977
Zaizen-aes2407 <168162069+Zaizen-aes2407@users.noreply.github.com>
2024-06-21 15:31:56 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.toc a');
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
