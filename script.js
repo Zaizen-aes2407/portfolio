bfbadfbe07ea5ae263f298120040270984f2ed3f
Zaizen-aes2407 <168162069+Zaizen-aes2407@users.noreply.github.com>
2024-06-21 16:30:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,4 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     });
+
+    const scrollToTopLink = document.getElementById('scroll-to-top');
+    scrollToTopLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
 });
